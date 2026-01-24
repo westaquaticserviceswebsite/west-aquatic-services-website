70be2355edbe62d64345f53e6faff97a89901466
david <davidgoose2000@gmail.com>
2026-01-24 05:48:57 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,9 @@ import Footer from '@/components/Footer';
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
-      <HeroSection heroMedia="https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=1600" />
+      <HeroSection heroMedia="https://images.unsplash.com/photo-1544551763-46a013bb70d5?w=1600" />
       
-      <StorySection storyMedia="https://images.unsplash.com/photo-1544551763-46a013bb70d5?w=800" />
+      <StorySection storyMedia="https://images.unsplash.com/photo-1582967788606-a171c1080cb0?w=800" />
       
       <WhyItWorksSection />
       
